0190eff6f93490d93777bd87fba6964d63394f61
zuzia <zalewska.zuzannaa@gmail.com>
2026-05-20 00:52:29 +0200

    komentarz

--- Projekt 3/utils/histogram_equalization.py
+++ Projekt 3/utils/histogram_equalization.py
@@ -1,152 +1,152 @@
-import numpy as np
+# import numpy as np
 
-#https://www.researchgate.net/profile/Nawaf-Hazim/publication/301693492_Fingerprint_Recognition_Improvement_Using_Histogram_Equalization_and_Compression_Methods/links/5723049108ae262228a5f7c9/Fingerprint-Recognition-Improvement-Using-Histogram-Equalization-and-Compression-Methods.pdf?origin=publication_detail&_tp=eyJjb250ZXh0Ijp7ImZpcnN0UGFnZSI6InB1YmxpY2F0aW9uIiwicGFnZSI6InB1YmxpY2F0aW9uRG93bmxvYWQiLCJwcmV2aW91c1BhZ2UiOiJwdWJsaWNhdGlvbiJ9fQ&__cf_chl_tk=B8vlUmnVp2DVqW1IEHwk1LGhim_bGWy0f_utc8SboCs-1778571971-1.0.1.1-0jM3pxjzWLq.CrA.4kWrafoDHrzHlzex1lVgzM9yrdQ
-#https://www.researchgate.net/profile/Cristinel-Mares/publication/44262481_Image_Enhancement_for_Fingerprint_Minutiae-Based_Algorithms_Using_CLAHE_Standard_Deviation_Analysis_and_Sliding_Neighborhood/links/00b7d525bfdc7e33f5000000/Image-Enhancement-for-Fingerprint-Minutiae-Based-Algorithms-Using-CLAHE-Standard-Deviation-Analysis-and-Sliding-Neighborhood.pdf?origin=publication_detail&_tp=eyJjb250ZXh0Ijp7ImZpcnN0UGFnZSI6InB1YmxpY2F0aW9uIiwicGFnZSI6InB1YmxpY2F0aW9uRG93bmxvYWQiLCJwcmV2aW91c1BhZ2UiOiJwdWJsaWNhdGlvbiJ9fQ&__cf_chl_tk=WN6tPeiivNxaSe24LYgX6FWxGhPGvJ53F3_faZRcWtk-1778585172-1.0.1.1-rXhaTfkJnHunC3UTG1rD6yEl3MUefs08EUADvAol8DI
-#https://www.acsu.buffalo.edu/~tulyakov/papers/Wu_MCRCS06_ImageQuality.pdf
-def histogram_equalization(img):
+# #https://www.researchgate.net/profile/Nawaf-Hazim/publication/301693492_Fingerprint_Recognition_Improvement_Using_Histogram_Equalization_and_Compression_Methods/links/5723049108ae262228a5f7c9/Fingerprint-Recognition-Improvement-Using-Histogram-Equalization-and-Compression-Methods.pdf?origin=publication_detail&_tp=eyJjb250ZXh0Ijp7ImZpcnN0UGFnZSI6InB1YmxpY2F0aW9uIiwicGFnZSI6InB1YmxpY2F0aW9uRG93bmxvYWQiLCJwcmV2aW91c1BhZ2UiOiJwdWJsaWNhdGlvbiJ9fQ&__cf_chl_tk=B8vlUmnVp2DVqW1IEHwk1LGhim_bGWy0f_utc8SboCs-1778571971-1.0.1.1-0jM3pxjzWLq.CrA.4kWrafoDHrzHlzex1lVgzM9yrdQ
+# #https://www.researchgate.net/profile/Cristinel-Mares/publication/44262481_Image_Enhancement_for_Fingerprint_Minutiae-Based_Algorithms_Using_CLAHE_Standard_Deviation_Analysis_and_Sliding_Neighborhood/links/00b7d525bfdc7e33f5000000/Image-Enhancement-for-Fingerprint-Minutiae-Based-Algorithms-Using-CLAHE-Standard-Deviation-Analysis-and-Sliding-Neighborhood.pdf?origin=publication_detail&_tp=eyJjb250ZXh0Ijp7ImZpcnN0UGFnZSI6InB1YmxpY2F0aW9uIiwicGFnZSI6InB1YmxpY2F0aW9uRG93bmxvYWQiLCJwcmV2aW91c1BhZ2UiOiJwdWJsaWNhdGlvbiJ9fQ&__cf_chl_tk=WN6tPeiivNxaSe24LYgX6FWxGhPGvJ53F3_faZRcWtk-1778585172-1.0.1.1-rXhaTfkJnHunC3UTG1rD6yEl3MUefs08EUADvAol8DI
+# #https://www.acsu.buffalo.edu/~tulyakov/papers/Wu_MCRCS06_ImageQuality.pdf
+# def histogram_equalization(img):
 
-    h, w = img.shape
-    N = h * w
+#     h, w = img.shape
+#     N = h * w
 
-    # 1. histogram n_k
-    n_k = np.zeros(256, dtype=np.int32)
+#     # 1. histogram n_k
+#     n_k = np.zeros(256, dtype=np.int32)
 
-    for y in range(h):
-        for x in range(w):
-            n_k[img[y, x]] += 1
+#     for y in range(h):
+#         for x in range(w):
+#             n_k[img[y, x]] += 1
 
-    # 2. probability density p_r(r_k)
-    p_r = n_k / N
+#     # 2. probability density p_r(r_k)
+#     p_r = n_k / N
 
-    # 3. cumulative distribution function (CDF)
-    cdf = np.zeros(256, dtype=np.float32)
-    cdf[0] = p_r[0]
+#     # 3. cumulative distribution function (CDF)
+#     cdf = np.zeros(256, dtype=np.float32)
+#     cdf[0] = p_r[0]
 
-    for k in range(1, 256):
-        cdf[k] = cdf[k - 1] + p_r[k]
+#     for k in range(1, 256):
+#         cdf[k] = cdf[k - 1] + p_r[k]
 
-    # 4. normalization (mapping to [0,255])
-    s_k = np.round(255 * cdf).astype(np.uint8)
+#     # 4. normalization (mapping to [0,255])
+#     s_k = np.round(255 * cdf).astype(np.uint8)
 
-    # 5. apply transform
-    out = np.zeros_like(img, dtype=np.uint8)
+#     # 5. apply transform
+#     out = np.zeros_like(img, dtype=np.uint8)
 
-    for y in range(h):
-        for x in range(w):
-            out[y, x] = s_k[img[y, x]]
+#     for y in range(h):
+#         for x in range(w):
+#             out[y, x] = s_k[img[y, x]]
 
-    return out
-
-
-def histogram_equalization_local(block):
-    """local HE for one tile"""
-
-    h, w = block.shape
-    N = h * w
-
-    hist = np.zeros(256, dtype=np.int32)
-
-    for y in range(h):
-        for x in range(w):
-            hist[block[y, x]] += 1
-
-    cdf = np.cumsum(hist) / N
-    lut = np.floor(255 * cdf).astype(np.uint8)
-
-    return lut[block]
+#     return out
 
 
-def clahe(img, tile_size=16, clip_limit=0.01):
+# def histogram_equalization_local(block):
+#     """local HE for one tile"""
 
-    img = img.astype(np.uint8)
+#     h, w = block.shape
+#     N = h * w
 
-    h, w = img.shape
+#     hist = np.zeros(256, dtype=np.int32)
 
-    out = np.zeros_like(img, dtype=np.uint8)
+#     for y in range(h):
+#         for x in range(w):
+#             hist[block[y, x]] += 1
 
-    # =========================
-    # number of tiles
-    # =========================
-    n_tiles_y = (h + tile_size - 1) // tile_size
-    n_tiles_x = (w + tile_size - 1) // tile_size
+#     cdf = np.cumsum(hist) / N
+#     lut = np.floor(255 * cdf).astype(np.uint8)
 
-    # each tile stores normalized CDF
-    tiles = np.zeros((n_tiles_y, n_tiles_x, 256), dtype=np.float32)
+#     return lut[block]
 
-    # ==========================================================
-    # 1. HISTOGRAM + CLIPPING + NORMALIZED CDF
-    # ==========================================================
-    for ty in range(n_tiles_y):
-        for tx in range(n_tiles_x):
 
-            y0 = ty * tile_size
-            x0 = tx * tile_size
+# def clahe(img, tile_size=16, clip_limit=0.01):
 
-            block = img[y0:y0 + tile_size, x0:x0 + tile_size]
+#     img = img.astype(np.uint8)
 
-            # =========================
-            # histogram
-            # =========================
-            hist = np.zeros(256, dtype=np.float32)
+#     h, w = img.shape
 
-            bh, bw = block.shape
+#     out = np.zeros_like(img, dtype=np.uint8)
 
-            for y in range(bh):
-                for x in range(bw):
-                    hist[block[y, x]] += 1
+#     # =========================
+#     # number of tiles
+#     # =========================
+#     n_tiles_y = (h + tile_size - 1) // tile_size
+#     n_tiles_x = (w + tile_size - 1) // tile_size
 
-            # =========================
-            # CLAHE clipping
-            # =========================
-            max_clip = clip_limit * block.size
+#     # each tile stores normalized CDF
+#     tiles = np.zeros((n_tiles_y, n_tiles_x, 256), dtype=np.float32)
 
-            excess = np.sum(np.maximum(hist - max_clip, 0))
+#     # ==========================================================
+#     # 1. HISTOGRAM + CLIPPING + NORMALIZED CDF
+#     # ==========================================================
+#     for ty in range(n_tiles_y):
+#         for tx in range(n_tiles_x):
 
-            hist = np.minimum(hist, max_clip)
+#             y0 = ty * tile_size
+#             x0 = tx * tile_size
 
-            # redistribute clipped pixels
-            hist += excess / 256.0
+#             block = img[y0:y0 + tile_size, x0:x0 + tile_size]
 
-            # =========================
-            # normalized CDF
-            # =========================
-            cdf = hist.cumsum()
+#             # =========================
+#             # histogram
+#             # =========================
+#             hist = np.zeros(256, dtype=np.float32)
 
-            if cdf[-1] != 0:
-                cdf = cdf / cdf[-1]
+#             bh, bw = block.shape
 
-            tiles[ty, tx] = cdf
+#             for y in range(bh):
+#                 for x in range(bw):
+#                     hist[block[y, x]] += 1
 
-    # ==========================================================
-    # 2. BILINEAR INTERPOLATION
-    # ==========================================================
-    for y in range(h):
-        for x in range(w):
+#             # =========================
+#             # CLAHE clipping
+#             # =========================
+#             max_clip = clip_limit * block.size
 
-            ty = y // tile_size
-            tx = x // tile_size
+#             excess = np.sum(np.maximum(hist - max_clip, 0))
 
-            # neighboring tiles
-            ty1 = min(ty + 1, n_tiles_y - 1)
-            tx1 = min(tx + 1, n_tiles_x - 1)
+#             hist = np.minimum(hist, max_clip)
 
-            # interpolation weights
-            fy = (y % tile_size) / tile_size
-            fx = (x % tile_size) / tile_size
+#             # redistribute clipped pixels
+#             hist += excess / 256.0
 
-            pixel = img[y, x]
+#             # =========================
+#             # normalized CDF
+#             # =========================
+#             cdf = hist.cumsum()
 
-            cdf00 = tiles[ty, tx][pixel]
-            cdf01 = tiles[ty, tx1][pixel]
-            cdf10 = tiles[ty1, tx][pixel]
-            cdf11 = tiles[ty1, tx1][pixel]
+#             if cdf[-1] != 0:
+#                 cdf = cdf / cdf[-1]
 
-            # bilinear interpolation
-            top = (1 - fx) * cdf00 + fx * cdf01
-            bottom = (1 - fx) * cdf10 + fx * cdf11
+#             tiles[ty, tx] = cdf
 
-            value = (1 - fy) * top + fy * bottom
+#     # ==========================================================
+#     # 2. BILINEAR INTERPOLATION
+#     # ==========================================================
+#     for y in range(h):
+#         for x in range(w):
 
-            out[y, x] = int(np.clip(value * 255, 0, 255))
+#             ty = y // tile_size
+#             tx = x // tile_size
 
-    return out
+#             # neighboring tiles
+#             ty1 = min(ty + 1, n_tiles_y - 1)
+#             tx1 = min(tx + 1, n_tiles_x - 1)
+
+#             # interpolation weights
+#             fy = (y % tile_size) / tile_size
+#             fx = (x % tile_size) / tile_size
+
+#             pixel = img[y, x]
+
+#             cdf00 = tiles[ty, tx][pixel]
+#             cdf01 = tiles[ty, tx1][pixel]
+#             cdf10 = tiles[ty1, tx][pixel]
+#             cdf11 = tiles[ty1, tx1][pixel]
+
+#             # bilinear interpolation
+#             top = (1 - fx) * cdf00 + fx * cdf01
+#             bottom = (1 - fx) * cdf10 + fx * cdf11
+
+#             value = (1 - fy) * top + fy * bottom
+
+#             out[y, x] = int(np.clip(value * 255, 0, 255))
+
+#     return out
